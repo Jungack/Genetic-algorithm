06a08b405f65fc4d04e73522fc3f2b7ede5bf5bd
Perrocheau Florian <florian.perrocheau@ensta-paris.fr>
2021-05-04 16:55:55 +0200

    New project version

--- TDD/unittests_Florian_Perrocheau.py
+++ TDD/unittests_Florian_Perrocheau.py
@@ -1,4 +1,6 @@
-# -*- coding: utf-8 -*-
+import unittest 
+target = __import__("project.py")
+
 """
 Created on Tue Apr 13 17:48:37 2021
 
--- project/methods.py
+++ project/methods.py
@@ -4,7 +4,5 @@ Pour coder les listes en binaire, on code les sous listes retenues (d'une liste
 initiale de taille n) comme une liste de taille n comportant des 0 aux indices correspondant
 aux indices des nombres non retenus de la liste initiale et des 1 aux indices correspondant 
 aux indices des nombres retenus de la liste initiale.
-=======
-Pour coder les listes en binaire, on code les sous listes retenues (d'une liste initiale de taille n) comme une liste de taille n comportant des 0 aux indices correspondant aux indices des nombres non retenus de la liste initiale et des 1 aux indices correspondant aux indices des nombres retenus de la liste initiale.
 >>>>>>> f799459cb5015b4dd8301f9827b46ff7e80d694b
 """

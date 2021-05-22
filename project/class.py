5da59963fbab8df3070f7694a3a8e91ae4dd64de
Perrocheau Florian <florian.perrocheau@ensta-paris.fr>
2021-05-22 19:53:21 +0200

    New project version

--- project/class.py
+++ project/class.py
@@ -14,11 +14,8 @@ class Population:
         self.nbOfIndividuals = 100
         self.l = len(initialList)
         self.individuals = 0 # useless to put It here since It will be defined with generateIndividuals method, but we want to keep a list of all the attributes of Population class
-<<<<<<< HEAD
         self.nbOfGenerations = 5000
-=======
-        self.nbOfGenerations = 1000
->>>>>>> ee58f692c83407d9578842e1e8e3ec184784a61f
+
 
 
     """
@@ -34,12 +31,9 @@ class Population:
             res += [Individual(temp, self.initialList)]
         self.individuals = res
 
-<<<<<<< HEAD
     def evaluateFitness(self, individual):
         return np.abs(np.dot(self.initialList, individual.geneticCode)) +10*(1-self.lenOfIndividual(individual)/self.l)
 
-=======
->>>>>>> ee58f692c83407d9578842e1e8e3ec184784a61f
     def setFitnessForOneIndividual(self, individual):
         individual.fitness = self.evaluateFitness(individual)
 
@@ -142,16 +136,8 @@ class Population:
         self.individuals here is sorted by fitness to make considering the lenght of each individuals faster.
         We define a best possible answer as the individual with the best fitness AND the longest lenght.
         """
-<<<<<<< HEAD
         if self.individuals[0].fitness < answer.fitness:
             answer = self.individuals[0]
-=======
-        i = 0
-        while (self.individuals[i].fitness <= answer.fitness) & (i < self.nbOfIndividuals):
-            if self.lenOfIndividual(self.individuals[i]) >= self.lenOfIndividual(answer):
-                answer = self.individuals[i]
-            i += 1
->>>>>>> ee58f692c83407d9578842e1e8e3ec184784a61f
         return answer
 
 
@@ -171,15 +157,11 @@ class Population:
                 self.mutateIndividuals(individual)
             self.setFitnessForAllIndividuals(tournamentSelectedIndividuals)
             self.createNewPopulationWithElitism(tournamentSelectedIndividuals)
-<<<<<<< HEAD
             self.updateBestAnswer(answer)
             print(self.sumOfList(answer))
             print(self.lenOfIndividual((answer)))
             answer = self.updateBestAnswer(answer)
         print(answer.geneticCode)
-=======
-            answer = self.updateBestAnswer(answer)
->>>>>>> ee58f692c83407d9578842e1e8e3ec184784a61f
         return answer
 
 
